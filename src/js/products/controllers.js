f3d105d89b3dcbf41e17b3016ce21b5c62d16d8c
Abdul Khan <akhan552@uwo.ca>
2016-03-12 18:29:01 -0500

    Fix frontend add product bug

--- src/js/products/controllers.js
+++ src/js/products/controllers.js
@@ -96,13 +96,8 @@ app.controller('ProductDisplayCtrl', ['$scope', 'Products', 'AuthService', '$sta
 ]);
 
 
-<<<<<<< HEAD
 app.controller('AddProductCtrl', ['$scope', 'Products', 'Upload', 'AuthService', '$state', '$uibModalInstance', '$http',
     function ($scope, Products, Upload, AuthService, $state, $uibModalInstance, $http) {
-=======
-app.controller('AddProductCtrl', ['$scope', 'Products', 'AuthService', '$state', '$uibModalInstance',
-    function($scope, Products, AuthService, $state, $uibModalInstance) {
->>>>>>> master
         $scope.fisheryName = "";
 
         Products.getProducts(function(products) {
@@ -113,114 +108,98 @@ app.controller('AddProductCtrl', ['$scope', 'Products', 'AuthService', '$state',
 
 
         // tied to ok button
-        $scope.ok = function() {
+        $scope.ok = function () {
+
+            console.log($scope.file);
 
             var data = {
                 name: $scope.name,
                 description: $scope.description,
                 unit: $scope.unit,
-                unitPrice: $scope.unitPrice
+                unitPrice: $scope.unitPrice,
+                fileName: $scope.file.name,
+                fileType: $scope.file.type,
+                fileSize: $scope.file.size
             };
 
             console.log("data");
             console.log(data);
 
-            Products.addProduct(data, function(res) {
-                $uibModalInstance.close(res);
-            }, function(err) {
-                $uibModalInstance.close(err);
-            })
+        	Products.addProduct(data, function (res) {
+        		$uibModalInstance.close(res);
+        	}, function (err) {
+        		$uibModalInstance.close(err);
+        	}).success(function(res) {
+                var payload = {
+                    url: res.signedUrl,
+                    data: $scope.file,
+                    headers: {
+                        'Content-Type': "image/png",
+                        'x-amz-acl': 'public-read',
+                    },
+                    ignoreInterceptor: true,
+                    method: "PUT"
+                };
 
-<<<<<<< HEAD
-        console.log($scope.file);
+                console.log(payload);
 
-        var data = {
-            name: $scope.name,
-            description: $scope.description,
-            unit: $scope.unit,
-            unitPrice: $scope.unitPrice,
-            fileName: $scope.file.name,
-            fileType: $scope.file.type,
-            fileSize: $scope.file.size
-=======
->>>>>>> master
+                Upload.http(payload);
+
+                // Upload.upload(payload).then(function (resp) {
+                //     // console.log('Success ' + resp.config.data.name + 'uploaded. Response: ' + resp.data);
+                // }, function (resp) {
+                //     // console.log('Error status: ' + resp.status);
+                // }, function (evt) {
+                //     // var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+                //     // console.log('progress: ' + progressPercentage + '% ' + evt.config.data.name);
+                // });
+
+
+
+                // console.log('here');
+                // console.log({
+                //     method: 'PUT',
+                //     url: res.signedUrl,
+                //     headers: {
+                //         'Content-Type': data.fileType,
+                //         'x-amz-acl': 'public-read'
+                //     },
+                //     body: data,
+                //     file: $scope.file,
+                //     ignoreInterceptor: true
+                // });
+                // $http({
+                //     method: 'PUT',
+                //     url: res.signedUrl,
+                //     headers: {
+                //         'Content-Type': data.fileType,
+                //         'x-amz-acl': 'public-read'
+                //     },
+                //     body: $scope.file,
+                //     file: $scope.file,
+                //     ignoreInterceptor: true
+                // })
+                // .then(function(res) {
+                //     console.log('img upload success!');
+                //     console.log(res);
+                // }, function(err) {
+                //     console.log('img upload fail');
+                //     console.log(err);
+                // });
+
+
+          });
+
         };
-
+        
         // tied to cancel button
         $scope.cancel = function() {
             $uibModalInstance.dismiss('cancel');
         };
 
 
-    }
-]);
+}]);
 
-<<<<<<< HEAD
-    	Products.addProduct(data, function (res) {
-    		$uibModalInstance.close(res);
-    	}, function (err) {
-    		$uibModalInstance.close(err);
-    	}).success(function(res) {
-            var payload = {
-                url: res.signedUrl,
-                data: $scope.file,
-                headers: {
-                    'Content-Type': "image/png",
-                    'x-amz-acl': 'public-read',
-                },
-                ignoreInterceptor: true,
-                method: "PUT"
-            };
-
-            console.log(payload);
-
-            Upload.http(payload);
-
-            // Upload.upload(payload).then(function (resp) {
-            //     // console.log('Success ' + resp.config.data.name + 'uploaded. Response: ' + resp.data);
-            // }, function (resp) {
-            //     // console.log('Error status: ' + resp.status);
-            // }, function (evt) {
-            //     // var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-            //     // console.log('progress: ' + progressPercentage + '% ' + evt.config.data.name);
-            // });
-
-
-
-            // console.log('here');
-            // console.log({
-            //     method: 'PUT',
-            //     url: res.signedUrl,
-            //     headers: {
-            //         'Content-Type': data.fileType,
-            //         'x-amz-acl': 'public-read'
-            //     },
-            //     body: data,
-            //     file: $scope.file,
-            //     ignoreInterceptor: true
-            // });
-            // $http({
-            //     method: 'PUT',
-            //     url: res.signedUrl,
-            //     headers: {
-            //         'Content-Type': data.fileType,
-            //         'x-amz-acl': 'public-read'
-            //     },
-            //     body: $scope.file,
-            //     file: $scope.file,
-            //     ignoreInterceptor: true
-            // })
-            // .then(function(res) {
-            //     console.log('img upload success!');
-            //     console.log(res);
-            // }, function(err) {
-            //     console.log('img upload fail');
-            //     console.log(err);
-            // });
-
-
-      });
-=======
 app.controller('DeleteProductCtrl', ['$scope', 'Products', 'AuthService', '$state', '$uibModalInstance',
     function($scope, Products, AuthService, $state, $uibModalInstance) {
         $scope.fisheryName = "";
@@ -229,7 +208,6 @@ app.controller('DeleteProductCtrl', ['$scope', 'Products', 'AuthService', '$stat
 
         // tied to ok button
         $scope.ok = function() {
->>>>>>> master
 
             Products.deleteProduct(Products.getSelectedProductId()).then(function(res) {
                 $uibModalInstance.close(res);
